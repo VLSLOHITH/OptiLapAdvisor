7d0dbf14534bf73ce6e577694bcd718fe635a877
VLS_LOHITH <84633352+VLSLOHITH@users.noreply.github.com>
2024-02-19 07:04:16 +0530

    Update App.py

--- UI/App.py
+++ UI/App.py
@@ -86,7 +86,7 @@ else:
 st.subheader("Explore the laptops below to see if any interest you, even though you haven't requested them")
 if st.checkbox("Laptops which are very low or very high conifgurations."):
     st.header("Extream Laptops")
-    Overal_Data[Overal_Data["cluster"]==4][:]
+    Overal_Data[Overal_Data["Cluster"]==4][:]
 
 
 
