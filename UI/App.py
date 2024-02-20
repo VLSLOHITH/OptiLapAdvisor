a7be09978f074feaf38a0ed42ca0e100e3206859
VLS_LOHITH <84633352+VLSLOHITH@users.noreply.github.com>
2024-02-20 14:37:58 +0530

    Update App.py

--- UI/App.py
+++ UI/App.py
@@ -78,15 +78,15 @@ try:
         PType_filter= webpage().Gen[flag] if flag else None
         Final_Result=Filter(Brand_filter,Processor_filter,RAM_filter,Storage_filter,Size_filter,Gaming_filter,FingerPrint_filter,OLED_filter,SSD_filter,Renewed_filter,PType_filter,Result)
         st.header("Result")   
-        st.dataframe(Final_Result.iloc[:,:-2],width=25600,hide_index=True)
+        st.dataframe(Final_Result.iloc[:,:-2],width=256000,hide_index=True)
     else:
         st.header("Result")   
-        st.dataframe(Result.iloc[:,:-2],width=25600,hide_index=True)
+        st.dataframe(Result.iloc[:,:-2],width=256000,hide_index=True)
     
     st.subheader("Explore the laptops below to see if any interest you, even though you haven't requested them")
     if st.checkbox("Laptops which are very low or very high conifgurations."):
         st.header("Extream Laptops")
-        st.dataframe(Overal_Data[Overal_Data["Cluster"]==4].iloc[:,:-2],width=25600,hide_index=True)
+        st.dataframe(Overal_Data[Overal_Data["Cluster"]==4].iloc[:,:-2],width=256000,hide_index=True)
 
 except:
     st.error("There is an error,we will look into it",icon="❗")
